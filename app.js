d81acf5fcff505a1ab0bf16e5fde320f9581b1b7
MaryNkoana01 <143197641+MaryNkoana01@users.noreply.github.com>
2024-02-14 11:47:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-function login(username, password) {
-  if (username === "admin" && password === "password123") {
-    localStorage.setItem("isLoggedIn", "true");
-    alert("Login successful!");
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Invalid username or password.");
-  }
-}
+[10:42] Lehlogonolo Mawasha [ MTN Group - South Africa ]
+var l = new URLSearchParams(window.location.search);
+
+var name = urlParams.get('name');
+ 
+var unsafe_div = window.document.getElementById("vulnerable-div");
+
+unsafe_div.innerHTML = "Hello " + name;
